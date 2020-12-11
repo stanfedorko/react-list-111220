bb7f060b393c55c9f894372ba3dce48797fd3919
Stas Fedorko <stasfedorko@MacBook-Pro-Stas.local>
2020-12-11 02:29:02 +0100

    deploy 111220-2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Comparator from './comparator.js';
-import CreditCapacity from './creditcapacity.js';
+// import CreditCapacity from './creditcapacity.js';
 
-function Home(){
-  return (
-    <div />
-  );
-}
+// function Home(){
+//   return (
+//     <div />
+//   );
+// }
 
 function App() {
   return (
-    <Router>
-      <Route path="/" component={Home} />
-      <Route path="/capacite-emprunt" component={CreditCapacity} />
-      <Route
-        path="/comparateur/:type"
-        component={Comparator}
-      />
-    </Router>
+    <Comparator/>
+    // <Router>
+    //   <Route path="/" component={Home} />
+    //   <Route path="/capacite-emprunt" component={CreditCapacity} />
+    //   <Route
+    //     path="/comparateur/:type"
+    //     component={Comparator}
+    //   />
+    // </Router>
   );
 }
 
