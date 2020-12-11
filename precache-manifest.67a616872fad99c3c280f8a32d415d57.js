5c108fae9e11a4e4320af578815f86ebdb3137ba
Stas Fedorko <stasfedorko@MacBook-Pro-Stas.local>
2020-12-11 02:19:44 +0100

    Updates

---
+++ precache-manifest.67a616872fad99c3c280f8a32d415d57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e67881a7044e11728b6d",
+    "url": "/react-list-111220/static/js/runtime~main.ca481016.js"
+  },
+  {
+    "revision": "fe47c622c9134c3e1558",
+    "url": "/react-list-111220/static/js/main.d49e5129.chunk.js"
+  },
+  {
+    "revision": "fefa344d99a5864f2a92",
+    "url": "/react-list-111220/static/js/2.933e856d.chunk.js"
+  },
+  {
+    "revision": "fe47c622c9134c3e1558",
+    "url": "/react-list-111220/static/css/main.0bbfe45a.chunk.css"
+  },
+  {
+    "revision": "41a09e02e451ba07efaf52ead648aae2",
+    "url": "/react-list-111220/index.html"
+  }
+];
